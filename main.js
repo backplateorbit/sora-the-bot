2f5bb06583596ec7e97c701301b403e249c81589
Backplate Orbit <backplateorbit@gmail.com>
2021-05-26 22:43:13 +0100

    testing the sora-bot-log channel webhook

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const client = new Discord.Client()
 
 client.on("ready", async() => {
     console.log("Sora Bot online and waiting for commands...");
-    const channels = await client.guilds.fetch(process.env.MATT_SERVER_ID)
-    console.log(channels)
 });
 
 client.login(process.env.BOT_KEY)
