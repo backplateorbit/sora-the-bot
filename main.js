829a39b69210c6d2dfcbc4262c7841f1ab1ea222
Backplate Orbit <backplateorbit@gmail.com>
2021-05-26 22:36:46 +0100

    added async keyword to the login function

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 
 const client = new Discord.Client()
 
-client.on("ready", () => {
+client.on("ready", async() => {
     console.log("Sora Bot online and waiting for commands...");
     const channels = await client.guilds.fetch(process.env.MATT_SERVER_ID)
     console.log(channels)
