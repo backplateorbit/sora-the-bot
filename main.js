8beee1b41410342a2a133e0d05f2d50a73df6284
Backplate Orbit <backplateorbit@gmail.com>
2021-05-26 22:35:43 +0100

    Added sample code to bot

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const client = new Discord.Client()
 
 client.on("ready", () => {
     console.log("Sora Bot online and waiting for commands...");
+    const channels = await client.guilds.fetch(process.env.MATT_SERVER_ID)
+    console.log(channels)
 });
 
 client.login(process.env.BOT_KEY)
