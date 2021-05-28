f0cad1c5ccd458620505fbb754c23f127a487a26
Backplate Orbit <backplateorbit@gmail.com>
2021-05-28 23:08:55 +0100

    trying to grab clips from clipchannel

--- main.js
+++ main.js
@@ -8,13 +8,19 @@ const authProvider = new twitchAuth.ClientCredentialsAuthProvider(
 )
 const apiClient = new twitch.ApiClient({authProvider})
 
+// Probes and then sends new clips to the clip channel.
+const fetchNewClips = async(clipChannel) => {
+    console.log("AH")
+}
+
 client.on("ready", async() => {
     const server = await client.guilds.fetch(process.env.MATT_SERVER_ID)
     const clipChannel = await server.channels.cache.find(
         (channel) => channel.id === process.env.MATT_CLIP_CHANNEL_ID
     )
-    console.log(server)
-    console.log(clipChannel)
+
+    const clipChannelMessages = await clipChannel.messages.fetch({limit: 0})
+    console.log(clipChannelMessages)
 });
 
 client.login(process.env.BOT_KEY)
