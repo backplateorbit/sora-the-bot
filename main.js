9bddd40db186bf912443c9dd1a6f46e53971db5b
Backplate Orbit <backplateorbit@gmail.com>
2021-05-28 23:02:25 +0100

    improvement

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ const authProvider = new twitchAuth.ClientCredentialsAuthProvider(
 const apiClient = new twitch.ApiClient({authProvider})
 
 client.on("ready", async() => {
-    const clipChannel = await client.guilds.fetch(
-        process.env.MATT_SERVER_ID
-    ).then(server => await server.channels.cache.find(
-        (channel => channel.id === process.env.MATT_CLIP_CHANNEL_ID))
+    const server = await client.guilds.fetch(process.env.MATT_SERVER_ID)
+    const clipChannel = await server.channels.cache.find(
+        (channel) => channel.id === process.env.MATT_CLIP_CHANNEL_ID
     )
+    console.log(server)
     console.log(clipChannel)
 });
 
