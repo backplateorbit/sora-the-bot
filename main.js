956543e53809693a6543d4004eb408afe2e734e2
Backplate Orbit <backplateorbit@gmail.com>
2021-05-28 22:50:47 +0100

    added twitch integration and testing clipchannel grabber

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const Discord = require("discord.js")
+import { ApiClient } from "twitch"
+import { ClientCredentialsAuthProvider } from "twitch-auth"
 
 const client = new Discord.Client()
+const authProvider = new ClientCredentialsAuthProvider(
+    process.env.TWITCH_CLIENT_ID, process.env.TWITCH_CLIENT_SERVER
+)
+const apiClient = new ApiClient({authProvider})
+
+const clipChannel = await client.guilds.fetch(process.env.MATT_SERVER_ID)
 
 client.on("ready", async() => {
-    console.log("Sora Bot online and waiting for commands...");
+    console.log(clipChannel)
 });
 
 client.login(process.env.BOT_KEY)
