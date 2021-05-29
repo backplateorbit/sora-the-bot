7b3ce6d40922a5e4f4fc0396d73d8f7677af5e01
Backplate Orbit <backplateorbit@gmail.com>
2021-05-29 14:31:49 +0100

    fixed role allocation, started stream online checker

--- main.js
+++ main.js
@@ -36,15 +36,47 @@ const fetchNewClips = async(server) => {
     
 }
 
+// Check Matt Streaming
+const checkMattStreaming = async(server) => {
+    // Get Matt's stream.
+    const stream = await apiClient.helix.streams.getStreamByUserName(process.env.MATT_TWITCH_USERNAME)
+
+    if (!stream) {
+        // Matt isn't streaming, no point doing anything else...
+        return
+    }
+
+    const streamStatusChannel = await server.channels.cache.get(process.env.MATT_STREAM_STATUS_CHANNEL_ID)
+
+    // Get the most recent message in the Status Channel.
+    const streamStatusChannelMessage = await streamStatusChannel.messages.fetch({limit: 1})
+
+    // We need to do a check to see if we've already announced this stream.
+    // If we have, we need to edit the message with any new stuff as an Embed.
+    
+    // Grab the message embed.
+    const streamStatusEmbed = streamStatusChannelMessage.embeds[0]
+    const streamStatusStreamId = streamStatusEmbed.fields.find((embedField) => embedField.name === "id").value
+
+    if (stream.id === streamStatusStreamId) {
+        // We have already announced the stream. We just need to update the Embed.
+    }
+
+
+
+    
+
 client.on("ready", async() => {
     const server = await client.guilds.fetch(process.env.MATT_SERVER_ID)
+
+    // Set up clip fetcher.
     setInterval(async() => await fetchNewClips(server), 600000)
 });
 
 // Move users who join the server to the right permission role.
 client.on("guildMemberAdd", async(member) => {
     const server = await client.guilds.fetch(process.env.MATT_SERVER_ID)
-    member.roles.add(server.roles.fetch("846675430408060938"))
+    member.roles.add(process.env.THE_BEYS_ROLE_ID)
     console.log(`New member ${member.displayName} added to The Beys.`)
 })
 
